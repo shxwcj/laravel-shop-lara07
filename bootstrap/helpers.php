d41c07f4a6b958c48915000a4da4e7a2bcf17d1b
v_vcjhwang <v_vcjhwang@tencent.com>
2020-07-24 17:08:16 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,12 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * 将当前请求路由转换为 CSS 类名称
+ * @return string|string[]|null
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
